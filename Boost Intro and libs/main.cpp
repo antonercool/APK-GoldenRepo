0d0046eda423898b1a8e7f1c5ef6ed4da4cd3018
anton <antonsihm@gmail.com>
2019-09-23 11:22:55 +0200

    make file edited

---
+++ Boost Intro and libs/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+
+
+
+int main (){
+
+    int test = 2;
+
+    std::cout << "hello world 1" << std::endl;
+    
+    std::cout << "hello world 2" << std::endl;
+    
+    std::cout << "hello world 3" << std::endl;
+    
+    std::cout << "hello world 4" << std::endl;
+
+    int number = 123;
+
+    std::cout << "your number " << number<< std::endl;
+
+
+
+
+
+}
