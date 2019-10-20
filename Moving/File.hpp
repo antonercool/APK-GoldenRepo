a37bca4eedad16d9bbacf205131d79c3e3907ed7
antonercool <antonsihm@gmail.com>
2019-10-20 15:45:48 +0200

    Implemented rule of 5 + test added

--- Moving/File.hpp
+++ Moving/File.hpp
@@ -3,19 +3,48 @@
 #include <stdio.h>
 #include <iostream>
 
+/*
+Simplified definition of lvalue and rvalues
+- lvaule - An object that occupies some identifiable location in memory
+    - most values is c++ are lvalues
+Example: 
+int i;          // i is a lvalue
+int* p = &i;    // u's address is identifiable
+i = 2           // memoty content is modified
+
+
+
+- rvalue - Any object that is not a lvalue (dont have a memlocation)
+Example:
+
+int i = 2           // i is a lvalue // 2 is a rvalue
+int x = i +2        // (i + 2) is a rvalue
+int* p = &(i + 2)   // error cant take address of rvalue
+i + 2 = 4           // error -- Left side of the assignment operator must ALLWAYS be a lvalue
+2  = i;             // error --   --|same ^|--                // right side can be rvalues and lvaues mixed 
+
+Car c1;             // lvalue
+c1 = Car();         // Car is a rvalue
+
+int i = sum(2,4)    // sum(2,4) is a rvalue
+*/
+
 namespace FileReader
 {
 class File
 {
 
 private:
-    FILE *_file;
+    FILE *_file = nullptr;
+    std::string _filePath;
 
 public:
     // Opens the file, with  read/write permissions
-    File(const std::string &file_path)
+    File(const std::string &filePath)
     {
-        _file = fopen(file_path.c_str(), "a+");
+        std::cout << "File Constructor" << std::endl;
+        _file = fopen(filePath.c_str(), "a+");
+        _filePath = filePath;
 
         if (_file == nullptr)
         {
@@ -23,22 +52,84 @@ public:
         }
     }
 
-    File(FileReader::File &&fileToShallowClone)
+    // CopyConstructor -- DeepCopy
+    File(const File &otherToCopy)
     {
-        //Steal resource from fileToShallowClone
-        this->_file = fileToShallowClone._file;
-        fileToShallowClone._file = nullptr;
+        std::cout << "CopyConstructor" << std::endl;
+        _file = fopen(otherToCopy._filePath.c_str(), "a+");
+        _filePath = otherToCopy._filePath;
     }
+
+    File &operator=(const File &otherToCopyAssign)
+    {
+
+        std::cout << "CopyAssigmentOperator" << std::endl;
+
+        if (this == &otherToCopyAssign)
+        {
+            return *this;
+        }
+
+        //Close this's currents file
+        if (_file != nullptr)
+        {
+            fclose(_file);
+        }
+        // can to obects read to and write the the same file ?? YES
+        _file = fopen(otherToCopyAssign._filePath.c_str(), "a+");
+        _filePath = otherToCopyAssign._filePath;
+        return *this;
+    }
+
+    // move constructor -- Shallow copy
+    File(FileReader::File &&fileToShallowClone) noexcept
+    {
+        std::cout << "MoveConstrutor" << std::endl;
+        //Steal resource from fileToShallowClone
+        if (_file != nullptr)
+        {
+            fclose(_file);
+            _file = nullptr;
+        }
+
+        _file = fileToShallowClone._file;
+        // When tempObject calls destructor if does not delete beacuse it is nullptr
+        fileToShallowClone._file = nullptr;
+        // std::swap(fileToShallowClone._file, this->_file)
+    }
+
+    File &operator=(const File &&fileToShallowAssign) noexcept
+    {
+        std::cout << "MoveAssigment" << std::endl;
+        //close own // and other to steal from
+        if (_file != nullptr)
+        {
+            fclose(_file);
+            fclose(fileToShallowAssign._file);
+        }
+
+        _file = fopen(fileToShallowAssign._filePath.c_str(), "a+");
+        _filePath = fileToShallowAssign._filePath;
+    }
+
+    // Important information about delete operation
+    // delete performs the check anyway, so checking it on your side adds overhead and looks uglier.
+    // A very good practice is setting the pointer to NULL after delete (helps avoiding double deletion and other similar memory corruption problems).
 
     // Close the file
     ~File()
     {
+        // If it is nullptr the resource has been moved
+        // Or the specified file was not found
         if (_file != nullptr)
         {
             fclose(_file);
             std::cout << "leasing resources" << std::endl;
         }
-        std::cout << "resources is empty, has been moved " << std::endl;
+        else
+        {
+            std::cout << "resources is empty, has been moved or the specified file was not found" << std::endl;
+        }
     }
 
     std::string readLine()
@@ -54,6 +145,18 @@ public:
     void appendLine(std::string stringToAdd)
     {
         fputs(stringToAdd.c_str(), _file);
+    }
+
+    bool holdsReso()
+    {
+        if (_file == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
 } // namespace FileReader
--- Moving/main.cpp
+++ Moving/main.cpp
@@ -2,16 +2,80 @@
 #include <string>
 #include "File.hpp"
 
+using namespace FileReader;
 
-int main(){
+int main()
+{
+    // Test Constructor -- Copy -- Assignment
+    std::cout << "Test Constructor -- Copy -- Assignment" << std::endl;
+    { // constructor
+        File myFile1("test.txt");
+        File myFile2("test2.txt");
 
-    FileReader::File myTestFile("test.txt");
+        //CopycConstructor
+        File myFile3(myFile1);
 
-    std::string first8chars = myTestFile.readLine();
-    std::cout << first8chars << std::endl;
+        std::string firstLine = myFile1.readLine();
+        std::string firstLine1 = myFile3.readLine();
 
-    myTestFile.readLine("Du er pisse grim");
-    
+        std::cout << firstLine << std::endl;
+        std::cout << firstLine1 << std::endl;
+
+        // Assignment
+        myFile2.appendLine("Musko");
+        // Should now use "Text2.txt"
+        myFile1 = myFile2;
+
+        // Should write "Musko"
+        std::cout << myFile1.readLine() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    // Testing Move semanticts
+    std::cout << "Testing copy Move Semantics" << std::endl;
+    {
+        // File("test2.txt") is a rvalue
+        File myFile1(std::move(File("test2.txt")));
+        //File myFile1(File{"test2.txt"});
+        ///File myFile1(File("test2.txt"));
+
+        std::cout << myFile1.readLine() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Testing assignment Move Semantics" << std::endl;
+    {
+        File myFile1("test.txt");
+
+        std::cout << myFile1.readLine() << std::endl;
+
+        //myFile1 = std::move(File("test2.txt"));
+        myFile1 = File("Test.txt");
+        //myFile1 = File{"test2.txt"};
+
+        std::cout << myFile1.readLine() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Testing valid move" << std::endl;
+    {
+        File myFile1("test.txt");
+        File myFile2("test2.txt");
+
+        std::cout << myFile1.readLine() << std::endl;
+
+        myFile1 = std::move(myFile2);
+
+        if (!myFile2.holdsReso())
+        {
+            std::cout << "File was succesfully moved" << std::endl;
+        }
+        
+        std::cout << myFile1.readLine() << std::endl;
+    }
+
+    getchar();
+
     return 0;
-
 }
